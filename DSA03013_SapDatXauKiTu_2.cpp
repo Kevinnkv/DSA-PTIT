88818d616c2bae3da2c6c6b54a89efea460c4d35
khacv <nguyenkhacvuong2003@gmail.com>
2023-03-30 22:39:29 +0700

    Sắp đặt xâu kí tự 2

---
+++ DSA03013_SapDatXauKiTu_2.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+ 
+using namespace std;
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    int t; cin >> t;
+    while(t--){
+        int d; cin >> d;
+        string s;
+        cin >> s;
+        int n=s.size();
+        map <char,int> mp; // <giá trị,số lần xuất hiện>
+        for(int i=0;i<n;i++){
+            mp[s[i]]++;
+        }
+        int check = 0;
+        for(auto it :mp){
+            if(it.second > ((n-1)/d)+1){
+                check = 1; break;
+            }
+             
+        }
+        if(check == 0) cout << "1" << endl;
+        else cout << "-1" << endl;
+    }
+    system("pause");
+    return 0;
+}
