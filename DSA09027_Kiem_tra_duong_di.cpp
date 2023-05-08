41c77ca7ad22656b8920929f9f1b88647daa2cf1
khacv <94861087+khacv@users.noreply.github.com>
2023-05-08 09:58:38 +0700

    Update DSA09027_Kiem_tra_duong_di.cpp

--- DSA09027_Kiem_tra_duong_di.cpp
+++ DSA09027_Kiem_tra_duong_di.cpp
@@ -40,3 +40,52 @@ int main(){
     system("pause");
     return 0;
 }
+// cách dùng tối ưu sử dụng Disjont set
+#include <iostream>
+#include <vector>
+using namespace std;
+
+const int MAXN = 1000;
+int parent[MAXN+1];
+
+// Hàm tìm tập hợp (set) chứa đỉnh u
+int find(int u) {
+    if (parent[u] == u) return u;
+    return parent[u] = find(parent[u]);
+}
+
+// Hàm gộp hai tập hợp chứa u và v thành một tập hợp
+void merge(int u, int v) {
+    int pu = find(u), pv = find(v);
+    if (pu != pv) parent[pu] = pv;
+}
+
+int main() {
+    int T; cin >> T;
+    while (T--) {
+        int n, m; cin >> n >> m;
+
+        // Khởi tạo mỗi đỉnh là một tập hợp độc lập
+        for (int i = 1; i <= n; i++) {
+            parent[i] = i;
+        }
+
+        // Gộp các tập hợp chứa đỉnh được nối bởi các cạnh
+        while (m--) {
+            int u, v; cin >> u >> v;
+            merge(u, v);
+        }
+
+        // Kiểm tra truy vấn
+        int q; cin >> q;
+        while (q--) {
+            int x, y; cin >> x >> y;
+            if (find(x) == find(y)) {
+                cout << "YES\n";
+            } else {
+                cout << "NO\n";
+            }
+        }
+    }
+    return 0;
+}
