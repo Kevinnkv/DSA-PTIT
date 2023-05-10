ada430905c49a7e7e5b0c789ae8f5f9fc70c6ba4
khacv <nguyenkhacvuong2003@gmail.com>
2023-05-10 17:39:34 +0700

    Dãy con tăng dần bậc k

---
+++ CTDL_004_Day_con_tang_dan_bac_k.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n,k,a[1000],X[1000];
+int cnt=0;
+vector <string> res;
+
+void Try(int i, int pos){
+	for(int j=pos; j<=n; j++){
+		if(a[j] > X[i-1]){// mang X lưu giá trị của phần tử i-1 rồi đem so sánh
+			X[i]=a[j];
+			if(i==k){
+				cnt++;
+			}
+			if(i < k) Try(i+1,j+1);
+		}
+	}
+}
+
+int main(){
+	cin>>n>>k;
+	for(int i=1; i<=n; i++) cin>>a[i];
+	Try(1,1);
+	cout<<cnt<<endl;
+	return 0;
+}
